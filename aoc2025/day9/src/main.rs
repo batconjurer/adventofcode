21ffb1067955370243703fc1037d5226a6508d35
satan <jacob.turner870@gmail.com>
2025-12-09 22:20:51 +0100

    optimize day 9

--- aoc2025/day9/src/main.rs
+++ aoc2025/day9/src/main.rs
@@ -9,37 +9,32 @@ fn area(point1: &Point, point2: &Point) -> u64 {
     width * height
 }
 
-/// Returns whether `point` is contained in the rectangle with
-/// `corner1` and `corner2` as opposite corners (being on the
-/// perimeter is returns false)
-fn contains(corner1: &Point, corner2: &Point, point: &Point) -> bool {
-    let row_min = std::cmp::min(corner1[0], corner2[0]);
-    let row_max = std::cmp::max(corner1[0], corner2[0]);
-    let col_min = std::cmp::min(corner1[1], corner2[1]);
-    let col_max = std::cmp::max(corner1[1], corner2[1]);
-    row_min < point[0] && point[0] < row_max && col_min < point[1] && point[1] < col_max
-}
-
 /// Check if a square formed by the provided corner contains any interior
 /// edge point in its interior
 fn interior_contains_boundary(corners: [&Point; 2], edge: [&Point; 2]) -> bool {
+    let row_min = std::cmp::min(corners[0][0], corners[1][0]);
+    let row_max = std::cmp::max(corners[0][0], corners[1][0]);
+    let col_min = std::cmp::min(corners[0][1], corners[1][1]);
+    let col_max = std::cmp::max(corners[0][1], corners[1][1]);
     // horizontal edge
     if edge[0][0] == edge[1][0] {
         let min_col = std::cmp::min(edge[0][1], edge[1][1]) + 1;
         let max_col = std::cmp::max(edge[0][1], edge[1][1]) - 1;
-        for col in min_col..=max_col {
-            if contains(corners[0], corners[1], &[edge[0][0], col]) {
-                return true
+
+        if row_min < edge[0][0] && edge[0][0] < row_max {
+            if (min_col <= col_min && col_min < max_col) || (col_min <= min_col && min_col <= col_max) {
+                return true;
             }
         }
+
         false
     } else {
         // vertical edge
         let min_row = std::cmp::min(edge[0][0], edge[1][0]) + 1;
         let max_row = std::cmp::max(edge[0][0], edge[1][0]) - 1;
-        for row in min_row..=max_row {
-            if contains(corners[0], corners[1], &[row, edge[0][1]]) {
-                return true
+        if col_min < edge[0][1] && edge[0][1] < col_max {
+            if (min_row <= row_min && row_min <= max_row) || (row_min <= min_row && min_row <= row_max) {
+                return true;
             }
         }
         false
@@ -86,11 +81,12 @@ fn part_two(filename: &str) {
     let num_points = points.len();
 
     for i in 0..num_points - 1 {
-         'outer: for j in i+1..num_points {
+         'outer: for j in i+2..num_points {
             for k in 0..num_points {
                 let edge = [&points[k], &points[(k + 1).rem_euclid(num_points)]];
                 // technically this is buggy if the polygon is sufficiently non-convex
-                // as it could find a large rectangle completely outside of the polygon
+                // as it could find a large rectangle completely outside the polygon.
+                // However, visual inspection reveals polygon is Pacman shaped.
                 if interior_contains_boundary([&points[i], &points[j]], edge) {
                     continue 'outer;
                 }
